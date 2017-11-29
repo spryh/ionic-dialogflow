9cc97102f7fb08124ede5bce61d79bab1f90d275
Paul Scarpa <spryh@Bootstrap.home>
2017-11-28 23:33:58 -0800

    thru 5.17 - Wired API.AI to home.html & changed relative path on PNG to work in Cordova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { HomePage } from '../pages/home/home';
+
+// window will allow us to use the browser's JS library
+declare var window
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -16,6 +19,15 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      window["ApiAIPlugin"].init(
+        {
+            clientAccessToken: "3b0c740693694c5cb33d756b58c14da4", // from https://console.dialogflow.com/ Gear icon
+            lang: "en" // set lang tag from list of supported languages 
+        }, 
+        function(result) { alert(result) },
+        function(error) { alert(error) }
+    );
     });
   }
 }
